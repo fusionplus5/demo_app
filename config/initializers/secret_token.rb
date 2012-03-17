7d1577d0e4837f934126ef2bdaa5f60e3fb40944
Your Name <fusionplus5@yahoo.com>
2012-03-17 12:47:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '68bfc83d72508eecd8b72e75f41cdb8f5fd8b11aa25299756291e44d8d037c3570a32874ef3746541e07d5d8773fe089fd49dc3d60d19a798637908c0c6ace57'
